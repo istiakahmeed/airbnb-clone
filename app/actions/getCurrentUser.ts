899d923661c398cca05a6deb2085c336a10b5b6c
Minal Ahmed <mimalahmed6219@gmail.com>
2024-04-06 16:28:10 +0600

    create type globally

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -10,19 +10,27 @@ export async function getSession() {
 export default async function getCurrentUser() {
   try {
     const session = await getSession();
-    if (!session) {
+
+    if (!session?.user?.email) {
       return null;
     }
 
     const currentUser = await prisma.user.findUnique({
       where: {
-        email: session.user?.email as string,
+        email: session.user.email as string,
       },
     });
+
     if (!currentUser) {
       return null;
     }
-    return currentUser;
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: currentUser.emailVerified?.toISOString() || null,
+    };
   } catch (error: any) {
     return null;
   }
